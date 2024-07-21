2002af897fcc2dd3c43ab693f39d40a5c2a8d997
Luis Mendoza Dev <lemt.09@gmail.com>
2024-07-21 15:15:04 -0500

    Primer commit

--- app/src/main/java/com/example/papeleriareto1/MainActivity.java
+++ app/src/main/java/com/example/papeleriareto1/MainActivity.java
@@ -1,14 +1,20 @@
 package com.example.papeleriareto1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    RecyclerView rv1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rv1=findViewById(R.id.rv1);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+
     }
 }
